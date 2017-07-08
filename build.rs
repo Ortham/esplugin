14610aa5d9ba144ae5f2da7f9aaefcca49b88af5
Oliver Hamlet <oliver.hamlet@gmail.com>
2017-07-08 21:13:07 +0100

    Autogenerate C and C++ headers using cbindgen
    Unfortunately it doesn't pick up on the constants, so they need to be
    explicitly inserted in.

---
+++ build.rs
@@ -0,0 +1,21 @@
+extern crate cbindgen;
+
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+
+fn main() {
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    fs::create_dir_all("include").expect("could not create include directory");
+
+    cbindgen::generate(&crate_dir).unwrap().write_to_file(
+        "include/libespm.h",
+    );
+
+    let mut config = cbindgen::Config::from_root_or_default(PathBuf::from(&crate_dir).as_path());
+    config.language = cbindgen::Language::Cxx;
+    cbindgen::generate_with_config(&crate_dir, &config)
+        .unwrap()
+        .write_to_file("include/libespm.hpp");
+}
